1ee3a1c76df5c216b05375b14e577495593376f2
Bradley Troll <bradleytroll@gmail.com>
2024-01-21 10:34:49 -0500

    Adds basic html, a div, and a start button. Adds an event listener to the start button in JavaScript.

---
+++ Develop/script.js
@@ -0,0 +1,11 @@
+// Grabs elements and assign variables
+var questionEl = document.getElementById("question-div");
+var startBtn = document.getElementById("start-button")
+
+
+// Adds event listeners
+startBtn.addEventListener("click", startQuiz);
+
+function startQuiz(event) {
+    console.log("button working")
+}
